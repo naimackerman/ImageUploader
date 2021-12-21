8d1e24d36c6b052bd4284c1859ff85700abbd9c0
naimackerman <naimackerman@gmail.com>
2021-12-22 05:55:08 +0700

    update UI

--- app/src/main/java/com/example/imageuploader/MainActivity.java
+++ app/src/main/java/com/example/imageuploader/MainActivity.java
@@ -1,250 +1,31 @@
 package com.example.imageuploader;
 
-import android.Manifest;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Base64;
-import android.util.Log;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.ml.vision.FirebaseVision;
-import com.google.firebase.ml.vision.common.FirebaseVisionImage;
-import com.google.firebase.ml.vision.text.FirebaseVisionText;
-import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
-
-import java.io.ByteArrayOutputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button b1, b2;
-    ImageView iv;
-    Bitmap bitmap;
-    TextView tv;
-    WebView wv;
-
-    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS= 7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        checkAndroidVersion();
 
-        b1 = findViewById(R.id.buttonFoto);
-        b2 = findViewById(R.id.buttonUpload);
-        iv = findViewById(R.id.imageView);
-        tv = findViewById(R.id.textId);
-        wv = findViewById(R.id.webView);
+        Button btnGo = findViewById(R.id.buttonGo);
 
-        b1.setOnClickListener(view -> {
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            launchActivity.launch(intent);
-        });
-
-        b2.setOnClickListener(view -> {
-            try {
-                uploadToServer();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
+        btnGo.setOnClickListener(operasi);
     }
 
-    ActivityResultLauncher<Intent> launchActivity = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
-                        Bundle bundle = result.getData().getExtras();
-                        bitmap = (Bitmap) bundle.get("data");
-                        iv.setImageBitmap(bitmap);
-
-                        FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(bitmap);
-                        FirebaseVision firebaseVision = FirebaseVision.getInstance();
-                        FirebaseVisionTextRecognizer firebaseVisionTextRecognizer = firebaseVision.getOnDeviceTextRecognizer();
-
-                        Task<FirebaseVisionText> task = firebaseVisionTextRecognizer.processImage(firebaseVisionImage);
-
-                        task.addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
-                            @Override
-                            public void onSuccess(FirebaseVisionText firebaseVisionText) {
-                                String s = firebaseVisionText.getText();
-                                String plat[] = s.split("\\r?\\n");
-
-                                tv.setText(plat[0]);
-
-                                String nopol = plat[0];
-
-                                wv.loadUrl("http://103.146.203.95/pajak/pkb.php?nopol=" + nopol);
-                            }
-                        });
-                        task.addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-                            }
-                        });
-                    }
-                }
-            }
-    );
-
-    public String BitMapToString() {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG,100,byteArrayOutputStream);
-        byte[] imgByte = byteArrayOutputStream.toByteArray();
-        return Base64.encodeToString(imgByte, Base64.DEFAULT);
-    }
-
-    public void uploadToServer() {
-        String image = BitMapToString();
-        ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
-        Call<ImgRes> call = apiInterface.uploadImage(image);
-
-        call.enqueue(new Callback<ImgRes>() {
-            @Override
-            public void onResponse(@NonNull Call<ImgRes> call, @NonNull Response<ImgRes> response) {
-                ImgRes imgRes = response.body();
-                assert imgRes != null;
-                Log.d("Server Response",""+imgRes.getResponse());
-            }
-
-            @Override
-            public void onFailure(@NonNull Call<ImgRes> call, @NonNull Throwable t) {
-                Log.d("Server Response",""+t.toString());
-            }
-        });
-    }
-
-    private void checkAndroidVersion() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            checkAndRequestPermissions();
-
-        } else {
-            // code for lollipop and pre-lollipop devices
+    View.OnClickListener operasi = v -> {
+        switch (v.getId()) {
+            case R.id.buttonGo:openCapture();break;
         }
+    };
 
-    }
-
-
-    private boolean checkAndRequestPermissions() {
-        int camera = ContextCompat.checkSelfPermission(MainActivity.this,
-                Manifest.permission.CAMERA);
-        int write = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        int read = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
-        List<String> listPermissionsNeeded = new ArrayList<>();
-        if (write != PackageManager.PERMISSION_GRANTED) {
-            listPermissionsNeeded.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        }
-        if (camera != PackageManager.PERMISSION_GRANTED) {
-            listPermissionsNeeded.add(Manifest.permission.CAMERA);
-        }
-        if (read != PackageManager.PERMISSION_GRANTED) {
-            listPermissionsNeeded.add(Manifest.permission.READ_EXTERNAL_STORAGE);
-        }
-        if (!listPermissionsNeeded.isEmpty()) {
-            ActivityCompat.requestPermissions(MainActivity.this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), REQUEST_ID_MULTIPLE_PERMISSIONS);
-            return false;
-        }
-        return true;
-    }
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        Log.d("in fragment on request", "Permission callback called-------");
-        switch (requestCode) {
-            case REQUEST_ID_MULTIPLE_PERMISSIONS: {
-
-                Map<String, Integer> perms = new HashMap<>();
-                // Initialize the map with both permissions
-                perms.put(Manifest.permission.WRITE_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
-                perms.put(Manifest.permission.CAMERA, PackageManager.PERMISSION_GRANTED);
-                perms.put(Manifest.permission.READ_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
-                // Fill with actual results from user
-                if (grantResults.length > 0) {
-                    for (int i = 0; i < permissions.length; i++)
-                        perms.put(permissions[i], grantResults[i]);
-                    // Check for both permissions
-                    if (perms.get(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
-                            && perms.get(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && perms.get(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                        Log.d("in fragment on request", "CAMERA & WRITE_EXTERNAL_STORAGE READ_EXTERNAL_STORAGE permission granted");
-                        // process the normal flow
-                        //else any one or both the permissions are not granted
-                    } else {
-                        Log.d("in fragment on request", "Some permissions are not granted ask again ");
-                        //permission is denied (this is the first time, when "never ask again" is not checked) so ask again explaining the usage of permission
-//                        // shouldShowRequestPermissionRationale will return true
-                        //show the dialog or snackbar saying its necessary and try again otherwise proceed with setup.
-                        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) || ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.CAMERA) || ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                            showDialogOK(
-                                    (dialog, which) -> {
-                                        switch (which) {
-                                            case DialogInterface.BUTTON_POSITIVE:
-                                                checkAndRequestPermissions();
-                                                break;
-                                            case DialogInterface.BUTTON_NEGATIVE:
-                                                // proceed with logic by disabling the related features or quit the app.
-                                                break;
-                                        }
-                                    });
-                        }
-                        //permission is denied (and never ask again is  checked)
-                        //shouldShowRequestPermissionRationale will return false
-                        else {
-                            Toast.makeText(MainActivity.this, "Go to settings and enable permissions", Toast.LENGTH_LONG)
-                                    .show();
-                            //                            //proceed with logic by disabling the related features or quit the app.
-                        }
-                    }
-                }
-            }
-        }
-
-    }
-
-    private void showDialogOK(DialogInterface.OnClickListener okListener) {
-        new AlertDialog.Builder(MainActivity.this)
-                .setMessage("Camera and Storage Permission required for this app")
-                .setPositiveButton("OK", okListener)
-                .setNegativeButton("Cancel", okListener)
-                .create()
-                .show();
+    private void openCapture() {
+        Intent intentku = new Intent(getBaseContext(), CaptureActivity.class);
+        startActivityForResult(intentku, 0);
     }
 }
